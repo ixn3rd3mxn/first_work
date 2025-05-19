8480e4aed81e4d329c8ddc09b4409f08ace39859
ixn3rd3mxn <ixn3rd3mxn@gmail.com>
2025-05-19 13:37:44 +0700

    .

---
+++ what_i_do.mk
@@ -0,0 +1,13 @@
+# WORK LOG ROADMAP
+
+## 1 Week
+- Learn & do function tool calling, FastAPI, LangChain, LangGraph
+
+## 2 Week
+- Test API product with JSON body
+- Learn & do MCP client & server
+- Try MCP client & server with LangChain adapter
+- Try MCP client & server with stdio & SSE
+
+## 3 Week
+- Learn & try backend/frontend code base product
